81e685e2489ce6a6d8d2b645978682d47c1bc9ba
Montgomery <92682714+MgtySickert@users.noreply.github.com>
2021-10-17 18:57:12 +0200

    SerialMonitorOutput
    Temperature, humidity and dewpoint ​​are displayed one below the other in the serial monitor.

--- SHT30_Arduino.cpp
+++ SHT30_Arduino.cpp
@@ -1,8 +1,7 @@
 #pragma once
-
 #include <Wire.h>
 
-#define Addr 0x44  // address is 0x44
+#define Addr 0x44   // address is 0x44
 #define B_CONST 237.3
 #define A_CONST 7.5
 
@@ -35,31 +34,33 @@ float calculate(float cTemp, float humidity)
 
 void setup()
 {
-  Wire.begin();
   // Initialise I2C communication
-  Serial.begin(9600);
+  Wire.begin();
   // Initialise serial communication, set baud rate = 9600
+  Serial.begin(9600);
   delay(300);
 }
 
-void loop()
-
+void loop() 
 {
   unsigned int data[6];
-  
+
   // Start I2C Transmission
-   Wire.beginTransmission(Addr);
+  Wire.beginTransmission(Addr);
+
   // Send measurement command
   Wire.write(0x2C);
   Wire.write(0x06);
-  Wire.endTransmission();
+
   // Stop I2C transmission
+  Wire.endTransmission();
   delay(500);
-  Wire.requestFrom(Addr, 6);
+
   // Request 6 bytes of data
-  if (Wire.available() == 6)
+  Wire.requestFrom(Addr, 6);
+
   // Read 6 bytes of data
- 
+  if (Wire.available() == 6)
   {
     data[0] = Wire.read();
     data[1] = Wire.read();
@@ -69,16 +70,22 @@ void loop()
     data[5] = Wire.read();
   }
 
+  // Convert the data
   float cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45;
   float humidity = ((((data[3] * 256.0) + data[4]) * 100) / 65535.0);
-  // Convert the data
-  
-  Serial.print("Humidity : ");
+  float dewPoint = calculate(cTemp, humidity);
+
+  // Output data to serial monitor
+  Serial.print("humidity : ");
   Serial.print(humidity);
   Serial.println(" %");
-  Serial.print("Temperature : ");
+  Serial.print("temperature : ");
   Serial.print(cTemp);
   Serial.println(" C");
+  Serial.print("dewPoint:");
+  Serial.print(dewPoint);
+  Serial.println(" C");
+  Serial.println("");
+  Serial.println("");
   delay(1000);
-  // Output data to serial monitor
 }
